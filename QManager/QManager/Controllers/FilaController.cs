3a68e8bec5eba597d2959582b8e5ecfb79900c9b
VSCordova <VSCordova@VSCORDOVA>
2016-06-16 00:04:30 +0000

    branch inicial

---
+++ QManager/QManager/Controllers/FilaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QManager.Controllers
+{
+    public class FilaController : Controller
+    {
+        // GET: Fila
+        public ActionResult Index()
+        {           
+            return View();
+        }
+    }
+}
